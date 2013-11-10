5564da2e0ed8b2087681c640038fbaafb00a1f10
Guillaume Coré (fridim) <g@fridim.org>
2013-11-10 23:54:14 +0000

    support racket-lang.org log format + optimizations
        * add options --name, to specify the channel name
        * optimization on main (for) (in-lines) loop

--- main.rkt
+++ main.rkt
@@ -11,34 +11,79 @@
          racket/date
          xml)
 
+(define weechat-mode (make-parameter #t))
+(define racket-lang-org-mode (make-parameter #f))
+(define chan-name (make-parameter ""))
 
-(define log-file (let ((p (command-line
-                            #:args (filename)
-                            filename)))
-                   (if (absolute-path? p)
-                     p
-                     (build-path (current-directory) p))))
+(define log-file
+  (let ((p (command-line
+             #:once-any
+             [("-w" "--weechat")
+              "input file is in weechat log format"
+              (weechat-mode #t)]
+             [("-r" "--racket-org")
+              "input file is in format as files on http://racket-lang.org/irc-logs/racket/"
+              (racket-lang-org-mode #t)]
+             #:once-each
+             [("-n" "--name") cn
+              "Name of the channel"
+              (chan-name cn)]
+             #:args (filename)
+             filename)))
+    (if (absolute-path? p)
+      p
+      (build-path (current-directory) p))))
 
 ; TODO: add auto fetch on scrolling (faster)
 ; TODO: add a checkbox to enable RAW format
 
-(module+ test
-         (require rackunit))
-
 ; weechat regex
-(define re-date #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})")
-(define re-saying #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t@?([^\\* -<][^\t]+)\t(.*)")
-(define re-me #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t \\*\t(.*)")
-(define re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t(<--|--|-->)\t(.*)")
+(define weechat-re-date   #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})")
+(define weechat-re-saying #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t@?([^\\* -<][^\t]+)\t(.*)")
+(define weechat-re-me     #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t \\*\t(.*)")
+(define weechat-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t(<--|--|-->)\t(.*)")
+
+(define racket-org-re-date   #px"^(\\d{4}-\\d{2}-\\d{2} \\d:\\d{2})")
+(define racket-org-re-saying #px"^(\\d{4}-\\d{2}-\\d{2} \\d:\\d{2}) ([^: ]+): (.*)")
+(define racket-org-re-me     #px"^(\\d{4}-\\d{2}-\\d{2} \\d:\\d{2}) ([^\\(: ]+) (.*)")
+(define racket-org-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d:\\d{2})( )(\\(join\\)|\\(quit\\)|\\(nick\\)|\\(topic\\)|\\(names\\)) (.*)")
 
 (module+ test
-         (check-equal? 4 (length (regexp-match re-saying "2013-10-20 18:03:32\tfridim\ttu quoque mi fili")))
-         (check-false (regexp-match? re-action "2013-10-20 18:03:32\tfridim\ttu quoque mi fili"))
-         (check-true (regexp-match? re-action "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
-         (check-true (regexp-match? re-me "2013-10-25 23:28:01\t *\tfridim sifflote"))
-         (check-false (regexp-match? re-saying "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
-         (check-true (regexp-match? re-action "2013-10-21 17:26:06\t-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil"))
-         (check-true (regexp-match? re-action "2013-10-23 14:35:10\t--\tMode #esil [+o meow`] par ChanServ")))
+         (require rackunit)
+         (check-equal? 4 (length (regexp-match weechat-re-saying "2013-10-20 18:03:32\tfridim\ttu quoque mi fili")))
+         (check-false (regexp-match? weechat-re-action "2013-10-20 18:03:32\tfridim\ttu quoque mi fili"))
+         (check-true (regexp-match? weechat-re-action "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
+         (check-true (regexp-match? weechat-re-me "2013-10-25 23:28:01\t *\tfridim sifflote"))
+         (check-false (regexp-match? weechat-re-saying "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
+         (check-true (regexp-match? weechat-re-action "2013-10-21 17:26:06\t-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil"))
+         (check-true (regexp-match? weechat-re-action "2013-10-23 14:35:10\t--\tMode #esil [+o meow`] par ChanServ"))
+         (check-true (regexp-match? racket-org-re-saying "2010-09-06 1:45 black_13: different paradigm"))
+         (check-true (regexp-match? racket-org-re-action "2010-09-06 1:47 (join) adadglgmut"))
+         (check-true (regexp-match? racket-org-re-me "2010-10-14 7:00 Lajla hides")))
+
+(define re-date
+  (cond ((racket-lang-org-mode)
+         racket-org-re-date)
+        (else
+          weechat-re-date)))
+
+(define re-saying
+  (cond ((racket-lang-org-mode)
+         racket-org-re-saying)
+        (else
+          weechat-re-saying)))
+
+(define re-me
+  (cond ((racket-lang-org-mode)
+         racket-org-re-me)
+        (else
+          weechat-re-me)))
+
+(define re-action
+  (cond ((racket-lang-org-mode)
+         racket-org-re-action)
+        (else
+          weechat-re-action)))
 
 (define (htmlize msg)
   (define (transform-url url)
@@ -63,18 +108,18 @@
       (string-replace date " " "") ":" ""))
 
   (define (get-logs from-s [to-s (current-seconds)])
-    (let ((from (seconds->date from-s))
-          (to (seconds->date to-s)))
+    (let ((from (date->string (seconds->date from-s)))
+          (to (date->string (seconds->date to-s))))
       `(table
          ,@(let ((in (open-input-file log-file)))
              (for*/list ((line (in-lines in))
+                         #:when (and (match? line)
+                                     (string>=? line from))
+                         #:break (string>? line to) 
                          (r-saying (in-value (regexp-match re-saying line)))
                          (r-action (in-value (regexp-match re-action line)))
                          (r-me (in-value (regexp-match re-me line)))
-                         #:when (and (string>=? line (date->string from))
-                                     (string<=? line (date->string to))
-                                     (or r-saying r-action r-me)
-                                     (match? line)))
+                         #:when (or r-saying r-action r-me))
                         (cond (r-saying
                                 (let ([date (cadr r-saying)]
                                       [nick (caddr r-saying)]
@@ -129,7 +174,8 @@
   (response/xexpr (site-dispatch request)))
 
 (define (index req)
-  (make-cdata #f #f (include-template "templates/index.html")))
+  (make-cdata #f #f (let ((channel (chan-name)))
+                      (include-template "templates/index.html"))))
 
 (module+ main
          (serve/servlet start
