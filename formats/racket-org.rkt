9a2e8f9e0b3d4c2b387650ce0c164e0c7efb1c62
Guillaume Coré (fridim) <g@fridim.org>
2014-02-09 12:44:12 -0500

    Add reverse procedure in formats : message->string
      First step to converter (WIP)

--- formats/racket-org.rkt
+++ formats/racket-org.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(provide racket-org-string->message)
+(provide racket-org-string->message
+         message->racket-org-string)
 
 (require "../message.rkt")
 
@@ -16,7 +17,7 @@
 
 (define racket-org-re-date   #px"^(\\d{4}-\\d{2}-\\d{2} \\d\\d:\\d{2})")
 (define racket-org-re-saying #px"^(\\d{4}-\\d{2}-\\d{2} \\d\\d:\\d{2}) ([^: ]+): (.*)")
-(define racket-org-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d\\d:\\d{2}) ((?:\\(join\\)|\\(part\\)|\\(notice\\)|\\(quit\\)|\\(nick\\)|\\(topic\\)|\\(names\\)) ([^: ]+).*)")
+(define racket-org-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d\\d:\\d{2}) (\\([^\\)]+\\) ([^: ]+).*)")
 (define racket-org-re-me     #px"^(\\d{4}-\\d{2}-\\d{2} \\d\\d:\\d{2}) ([^ ]+) (.*)")
 
 (define (racket-org-string->message line)
@@ -34,13 +35,28 @@
               (list->message r-me 'me)
               (raise (string-append "no match for line: " line)))))))))
 
+(define (message->racket-org-string m)
+  (string-append (message-date m) " "
+                 (let ((mt (message-type m)))
+                   (cond
+                     ((eq? mt 'saying)
+                      (string-append (message-nick m)
+                                     ": "
+                                     (message-msg m)))
+                     ((eq? mt 'action)
+                      (string-append (message-msg m)))
+                     ((eq? mt 'me)
+                      (string-append (message-nick m)
+                                     " "
+                                     (message-msg m)))))))
+
 (module+ test
          (require rackunit)
          (check-true (regexp-match? racket-org-re-saying "2010-09-06 21:45 black_13: different paradigm"))
          (check-true (regexp-match? racket-org-re-action "2010-09-06 11:47 (join) adadglgmut"))
          (check-true (regexp-match? racket-org-re-me "2010-10-14 07:00 Lajla hides"))
-         (check-equal?  (racket-org-string->message "2010-05-30 00:35 (topic) -: Racket: http://racket-lang.org")
-                        (message "2010-05-30 00:35" 'action "-" "(topic) -: Racket: http://racket-lang.org"))
+         (check-equal? (racket-org-string->message "2010-05-30 00:35 (topic) -: Racket: http://racket-lang.org")
+                       (message "2010-05-30 00:35" 'action "-" "(topic) -: Racket: http://racket-lang.org"))
          (check-equal? (racket-org-string->message "2011-01-12 21:37 _p4bl0: bremner: geiser rocks")
                        (message "2011-01-12 21:37" 'saying "_p4bl0" "bremner: geiser rocks"))
          (check-equal? (racket-org-string->message "2012-05-10 00:20 (quit) kreol[Ukr]: Ping timeout: 252 seconds")
@@ -60,4 +76,38 @@
          (check-equal? (racket-org-string->message "2013-11-13 08:24 (join) yacks")
                        (message "2013-11-13 08:24" 'action "yacks" "(join) yacks"))
          (check-equal? (racket-org-string->message "2013-10-31 00:09 (quit) rmathews: Quit: Bye..")
-                       (message "2013-10-31 00:09" 'action "rmathews" "(quit) rmathews: Quit: Bye..")))
+                       (message "2013-10-31 00:09" 'action "rmathews" "(quit) rmathews: Quit: Bye.."))
+
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2010-05-30 00:35 (topic) -: Racket: http://racket-lang.org"))
+                       "2010-05-30 00:35 (topic) -: Racket: http://racket-lang.org")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2011-01-12 21:37 _p4bl0: bremner: geiser rocks"))
+                       "2011-01-12 21:37 _p4bl0: bremner: geiser rocks")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2012-05-10 00:20 (quit) kreol[Ukr]: Ping timeout: 252 seconds"))
+                       "2012-05-10 00:20 (quit) kreol[Ukr]: Ping timeout: 252 seconds")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2012-10-02 16:00 (part) _tca"))
+                       "2012-10-02 16:00 (part) _tca")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2012-10-02 16:00 ozzloy: oh hey, people are here"))
+                       "2012-10-02 16:00 ozzloy: oh hey, people are here")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2012-10-02 16:00 bremner hides"))
+                       "2012-10-02 16:00 bremner hides")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2012-10-02 16:01 ozzloy: regarding https://github.com/plt/racket/wiki/Racket2 i recall one feature discussed was using #:else instead of [else ...]"))
+                       "2012-10-02 16:01 ozzloy: regarding https://github.com/plt/racket/wiki/Racket2 i recall one feature discussed was using #:else instead of [else ...]")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2013-11-13 08:19 (quit) statonjr: Ping timeout: 246 seconds"))
+                       "2013-11-13 08:19 (quit) statonjr: Ping timeout: 246 seconds")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2013-11-13 08:19 (nick) statonjr_ -> statonjr"))
+                       "2013-11-13 08:19 (nick) statonjr_ -> statonjr")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2013-11-13 08:24 (join) yacks"))
+                       "2013-11-13 08:24 (join) yacks")
+         (check-equal? (message->racket-org-string
+                         (racket-org-string->message "2013-10-31 00:09 (quit) rmathews: Quit: Bye.."))
+                       "2013-10-31 00:09 (quit) rmathews: Quit: Bye.."))
--- formats/weechat.rkt
+++ formats/weechat.rkt
@@ -3,7 +3,8 @@
 (require "../message.rkt"
          racket/list)
 
-(provide weechat-string->message)
+(provide weechat-string->message
+         message->weechat-string)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Weechat log format
@@ -18,7 +19,7 @@
 (define weechat-re-date   #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})")
 (define weechat-re-saying #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t@?([^ -<][^\t]+)\t(.*)")
 (define weechat-re-me     #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t \\*\t([^ ]+) (.*)")
-(define weechat-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t(<--|-->)\t([^ ]+) (.*)")
+(define weechat-re-action #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t(<--|-->)\t(([^ ]+) .*)")
 (define weechat-re-info   #px"^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\t--()\t(.*)")
 
 (define (weechat-string->message line)
@@ -27,9 +28,11 @@
       (list->message r-saying 'saying)
       (let ((r-action (regexp-match weechat-re-action line)))
         (if r-action
-          (let* ([date (cadr r-action)]
-                 [nick (cadddr r-action)]
-                 [msg (string-append (caddr r-action) " " nick " " (last r-action))])
+          (let ([date (cadr r-action)]
+                [nick (last r-action)]
+                [msg (string-append
+                        (caddr r-action) "\t"
+                        (cadddr r-action))])
             (message date 'action nick msg))
           (let ((r-me (regexp-match weechat-re-me line)))
             (if r-me
@@ -39,27 +42,64 @@
                   (list->message r-info 'info)
                   (raise (string-append "no match for line: " line)))))))))))
 
+(define (message->weechat-string m)
+  (string-append (message-date m) "\t"
+                 (let ((mt (message-type m)))
+                   (cond
+                     ((eq? mt 'saying)
+                      (string-append (message-nick m)
+                                     "\t"
+                                     (message-msg m)))
+                     ((eq? mt 'action)
+                      (string-append (message-msg m)))
+                     ((eq? mt 'me)
+                      (string-append " *\t"
+                                     (message-nick m)
+                                     " "
+                                     (message-msg m)))
+                     ((eq? mt 'info)
+                      (string-append "--\t"
+                                     (message-msg m)))))))
+
 (module+ test
          (require rackunit)
          (check-equal? (weechat-string->message "2013-10-20 18:03:32\tfridim\ttu quoque mi fili")
                        (message "2013-10-20 18:03:32" 'saying "fridim" "tu quoque mi fili"))
          (check-equal? (weechat-string->message "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)")
-                       (message "2013-10-20 18:32:05" 'action "v0n" "<-- v0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
+                       (message "2013-10-20 18:32:05" 'action "v0n" "<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
          (check-equal? (weechat-string->message "2013-10-25 23:28:01\t *\tfridim sifflote")
                        (message "2013-10-25 23:28:01" 'me "fridim" "sifflote"))
          (check-equal? (weechat-string->message "2013-10-23 14:35:10\t--\tMode #esil [+o meow`] par ChanServ")
                        (message "2013-10-23 14:35:10" 'info "" "Mode #esil [+o meow`] par ChanServ"))
          (check-equal? (weechat-string->message "2013-10-21 17:26:06\t-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil")
-                       (message "2013-10-21 17:26:06" 'action "v0n" "--> v0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil"))
+                       (message "2013-10-21 17:26:06" 'action "v0n" "-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil"))
          (check-equal? (message-type
                          (weechat-string->message "2013-11-01 20:03:46	<--	add^_ (~user@m5-241-189-182.cust.tele2.se) a quitté (Quit: ERC Version 5.3 (IRC client for Emacs))"))
                        'action)
          (check-equal? (weechat-string->message "2013-11-01 20:06:25	-->	gcartier (~gcartier@modemcable010.136-201-24.mc.videotron.ca) a rejoint #scheme")
-                       (message "2013-11-01 20:06:25" 'action "gcartier" "--> gcartier (~gcartier@modemcable010.136-201-24.mc.videotron.ca) a rejoint #scheme"))
+                       (message "2013-11-01 20:06:25" 'action "gcartier" "-->\tgcartier (~gcartier@modemcable010.136-201-24.mc.videotron.ca) a rejoint #scheme"))
          (check-equal? (weechat-string->message "2013-11-01 20:06:51	--	AlterSid est maintenant connu sous le nom Guest99037")
                        (message "2013-11-01 20:06:51" 'info "" "AlterSid est maintenant connu sous le nom Guest99037"))
          (check-equal? (weechat-string->message "2013-11-01 20:24:25	LeoNerd	(EVERYBODY LOVES SHOUTY 'LISP)")
                        (message "2013-11-01 20:24:25" 'saying "LeoNerd" "(EVERYBODY LOVES SHOUTY 'LISP)"))
          (check-equal? (weechat-string->message "2013-10-30 16:28:15	--	v0n est maintenant connu sous le nom v0n|w")
-                       (message "2013-10-30 16:28:15" 'info "" "v0n est maintenant connu sous le nom v0n|w")))
+                       (message "2013-10-30 16:28:15" 'info "" "v0n est maintenant connu sous le nom v0n|w"))
 
+         (check-equal? (message->weechat-string (message "2013-10-20 18:03:32" 'saying "fridim" "tu quoque mi fili"))
+                       "2013-10-20 18:03:32\tfridim\ttu quoque mi fili")
+         (check-equal? (message->weechat-string (message "2013-10-20 18:32:05" 'action "v0n" "<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)"))
+                       "2013-10-20 18:32:05\t<--\tv0n (~vivien@96.127.192.36) a quitté (Ping timeout: 245 seconds)")
+         (check-equal? (message->weechat-string (message "2013-10-25 23:28:01" 'me "fridim" "sifflote"))
+                       "2013-10-25 23:28:01\t *\tfridim sifflote")
+         (check-equal? (message->weechat-string (message "2013-10-23 14:35:10" 'info "" "Mode #esil [+o meow`] par ChanServ"))
+                       "2013-10-23 14:35:10\t--\tMode #esil [+o meow`] par ChanServ")
+         (check-equal? (message->weechat-string (message "2013-10-21 17:26:06" 'action "v0n" "-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil"))
+                       "2013-10-21 17:26:06\t-->\tv0n (~vivien@mtl.savoirfairelinux.net) a rejoint #esil")
+         (check-equal? (message->weechat-string (message "2013-11-01 20:06:25" 'action "gcartier" "-->\tgcartier (~gcartier@modemcable010.136-201-24.mc.videotron.ca) a rejoint #scheme"))
+                       "2013-11-01 20:06:25	-->	gcartier (~gcartier@modemcable010.136-201-24.mc.videotron.ca) a rejoint #scheme")
+         (check-equal? (message->weechat-string (message "2013-11-01 20:06:51" 'info "" "AlterSid est maintenant connu sous le nom Guest99037"))
+                       "2013-11-01 20:06:51	--	AlterSid est maintenant connu sous le nom Guest99037")
+         (check-equal? (message->weechat-string (message "2013-11-01 20:24:25" 'saying "LeoNerd" "(EVERYBODY LOVES SHOUTY 'LISP)"))
+                       "2013-11-01 20:24:25	LeoNerd	(EVERYBODY LOVES SHOUTY 'LISP)")
+         (check-equal? (message->weechat-string (message "2013-10-30 16:28:15" 'info "" "v0n est maintenant connu sous le nom v0n|w"))
+                       "2013-10-30 16:28:15	--	v0n est maintenant connu sous le nom v0n|w"))
