06d2b02bb4d35d4e589d7a414ac54d9df7414d28
Guillaume Coré (fridim) <g@fridim.org>
2014-03-09 17:11:18 -0400

    add version and deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,12 @@
-#lang info
+#lang setup/infotab
 
+(define version "0.1")
 (define collection "pimpmylog")
+
+(define deps '("base"
+               "srfi-lite-lib"
+               "web-server-lib"))
+
+(define build-deps '("rackunit-lib"))
 
 (define raco-commands '(("pimpmylog" (submod pimpmylog main) "run pimpmylog" #f)))
